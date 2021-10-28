d9313c3fd58e7730af2107db6ac48426fb0cb689
Swati Karot <v-skarot@microsoft.com>
2021-10-29 00:00:41 +0530

    added button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}
+  <button type="button">Click Me!</button>
+  </div>;
 }
 
 export default App;
